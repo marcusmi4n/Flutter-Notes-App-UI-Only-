13505d459c726b52801aaad968e106ea0b14adf5
marcusmi4n <marcuslutas@gmail.com>
2025-06-15 23:51:05 +0300

    debug

--- build/linux/x64/debug/cmake_install.cmake
+++ build/linux/x64/debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/grimian/Documents/OPP/notes_app/linux
+# Install script for directory: /home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/linux
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle")
+  set(CMAKE_INSTALL_PREFIX "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -44,96 +44,96 @@ endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
   
-  file(REMOVE_RECURSE "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/")
+  file(REMOVE_RECURSE "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/")
   
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app")
+  if(EXISTS "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app")
     file(RPATH_CHECK
-         FILE "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app"
+         FILE "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app"
          RPATH "$ORIGIN/lib")
   endif()
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app")
+   "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle" TYPE EXECUTABLE FILES "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/intermediates_do_not_run/notes_app")
-  if(EXISTS "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app")
+  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle" TYPE EXECUTABLE FILES "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/intermediates_do_not_run/notes_app")
+  if(EXISTS "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app")
     file(RPATH_CHANGE
-         FILE "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app"
-         OLD_RPATH "/home/grimian/Documents/OPP/notes_app/linux/flutter/ephemeral:"
+         FILE "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app"
+         OLD_RPATH "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/linux/flutter/ephemeral:"
          NEW_RPATH "$ORIGIN/lib")
     if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/usr/bin/llvm-strip" "$ENV{DESTDIR}/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/notes_app")
+      execute_process(COMMAND "/usr/bin/llvm-strip" "$ENV{DESTDIR}/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/notes_app")
     endif()
   endif()
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/data/icudtl.dat")
+   "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/data/icudtl.dat")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/data" TYPE FILE FILES "/home/grimian/Documents/OPP/notes_app/linux/flutter/ephemeral/icudtl.dat")
+  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/data" TYPE FILE FILES "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/linux/flutter/ephemeral/icudtl.dat")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/lib/libflutter_linux_gtk.so")
+   "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/lib/libflutter_linux_gtk.so")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/lib" TYPE FILE FILES "/home/grimian/Documents/OPP/notes_app/linux/flutter/ephemeral/libflutter_linux_gtk.so")
+  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/lib" TYPE FILE FILES "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/linux/flutter/ephemeral/libflutter_linux_gtk.so")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/lib/")
+   "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/lib/")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/lib" TYPE DIRECTORY FILES "/home/grimian/Documents/OPP/notes_app/build/native_assets/linux/")
+  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/lib" TYPE DIRECTORY FILES "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/native_assets/linux/")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
   
-  file(REMOVE_RECURSE "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/data/flutter_assets")
+  file(REMOVE_RECURSE "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/data/flutter_assets")
   
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Runtime" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/data/flutter_assets")
+   "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/data/flutter_assets")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle/data" TYPE DIRECTORY FILES "/home/grimian/Documents/OPP/notes_app/build//flutter_assets")
+  file(INSTALL DESTINATION "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle/data" TYPE DIRECTORY FILES "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build//flutter_assets")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/flutter/cmake_install.cmake")
-  include("/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/runner/cmake_install.cmake")
+  include("/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/flutter/cmake_install.cmake")
+  include("/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/runner/cmake_install.cmake")
 
 endif()
 
@@ -145,5 +145,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/linux/x64/debug/runner/cmake_install.cmake
+++ build/linux/x64/debug/runner/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/grimian/Documents/OPP/notes_app/linux/runner
+# Install script for directory: /home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/linux/runner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/grimian/Documents/OPP/notes_app/build/linux/x64/debug/bundle")
+  set(CMAKE_INSTALL_PREFIX "/home/grimian/Documents/OPP/Flutter-Notes-App-UI-Only-/build/linux/x64/debug/bundle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
